496075f824fcbd30dd32f37c65dfbaeb42c7d621
oneyian <wya@oneyian.cn>
2018-02-24 12:01:19 +0800

    模板文字

--- ModelCard/AddUserInfo/Controller/ModelImageController.m
+++ ModelCard/AddUserInfo/Controller/ModelImageController.m
@@ -73,6 +73,29 @@
         firstView.tag = i+1;
         [self.BackView addSubview:firstView];
     }
+    NSArray *points = [[NSArray alloc]initWithArray:self.modelDictionary[@"viewPoint"]];
+    for (int i=0; i<points.count; i++) {
+        NSArray *aArray = [self loadModelData:points[i]];
+        UILabel *title = [[UILabel alloc]initWithFrame:CGRectMake([aArray[0] floatValue], [aArray[1] floatValue], [aArray[2] floatValue], [aArray[3] floatValue])];
+        title.textAlignment = NSTextAlignmentCenter;
+        title.text = [NSString stringWithFormat:@"%d",i];
+        switch (i) {
+            case 0:
+                title.textColor = [UIColor redColor];
+                break;
+            case 1:
+                title.textColor = [UIColor blueColor];
+                break;
+            case 2:
+                title.textColor = [UIColor cyanColor];
+                break;
+            default:
+                title.textColor = [UIColor whiteColor];
+                break;
+        }
+        [title makeBorderWidth:i+2 withColor:title.textColor];
+        [self.BackView addSubview:title];
+    }
 }
 #pragma mark  =========== 导航栏View按钮 ==========
 -(void)back:(UIButton *)sender{
@@ -82,15 +105,16 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 -(void)done:(UIButton *)sender{
-    
-}
--(void)edit:(UIButton *)sender{
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"是否保存到相册" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"保存提示" message:@"您需要保存到相册？" preferredStyle:UIAlertControllerStyleAlert];
     [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:nil]];
     [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         UIImageWriteToSavedPhotosAlbum([self screenShotView:self.BackView], self, @selector(image:didFinishSavingWithError:contextInfo:), (__bridge void *)self);
     }]];
     [self presentViewController:alert animated:YES completion:nil];
+}
+-(void)edit:(UIButton *)sender{
+    sender.selected = !sender.isSelected;
+    self.BackView.backgroundColor = sender.isSelected ? [UIColor blackColor]:[UIColor whiteColor];
 }
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo{
     NSString *status = nil;
@@ -136,8 +160,7 @@
                         break;
                     }
                     if ([self.selectArray.firstObject intValue] == i+1) continue;
-                    view.layer.borderWidth = 3;
-                    view.layer.borderColor = [UIColor colorHex:@"#E7586E"].CGColor;
+                    [view makeBorderWidth:3 withColor:[UIColor colorHex:@"#E7586E"]];
                 }
             }
         }
